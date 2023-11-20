b233acae195e7dbf78d93a5e68a8926ec4e3803f
adeiskandarzulkarnaen <adeiskandarzulkarnaen@gmail.com>
2023-11-20 23:58:43 +0700

    fix test case

--- src/Applications/use_case/_test/AddCommentUseCase.test.js
+++ src/Applications/use_case/_test/AddCommentUseCase.test.js
@@ -34,7 +34,7 @@ describe('AddCommentUseCase', () => {
 
     // Assert
     expect(addedComment).toStrictEqual(new AddedComment({
-      id: 'comment-12',
+      id: 'comment-123',
       content: 'A comment',
       owner: 'user-123',
     }));
