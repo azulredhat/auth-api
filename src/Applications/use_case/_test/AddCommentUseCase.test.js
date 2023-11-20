01ff0683a66135aa475f88e8ebc8fe6bf8ea2173
adeiskandarzulkarnaen <adeiskandarzulkarnaen@gmail.com>
2023-11-20 22:12:22 +0700

    add test case
    failed

--- src/Applications/use_case/_test/AddCommentUseCase.test.js
+++ src/Applications/use_case/_test/AddCommentUseCase.test.js
@@ -34,7 +34,7 @@ describe('AddCommentUseCase', () => {
 
     // Assert
     expect(addedComment).toStrictEqual(new AddedComment({
-      id: 'comment-123',
+      id: 'comment-12',
       content: 'A comment',
       owner: 'user-123',
     }));
